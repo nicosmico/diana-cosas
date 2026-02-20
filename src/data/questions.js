d339720d83c348304e3e4fb08672ff1fc4f7f926
nicosmico <niccolas.dev@gmail.com>
2026-02-19 22:55:30 -0300

    feat: Add initial quiz questions data.

--- src/data/questions.js
+++ src/data/questions.js
@@ -2,19 +2,55 @@ export const questions = [
     {
         id: 1,
         question: "¿Cual es mi color favorito?",
-        options: ["Rojo", "Azul", "Rosa", "Verde"],
-        correctAnswer: "Rosa"
+        options: ["Rojo", "Naranjo", "Amarillo", "Celeste"],
+        correctAnswer: "Naranjo"
     },
     {
         id: 2,
-        question: "¿Que comida prefiero un viernes por la noche?",
-        options: ["Pizza", "Sushi", "Tacos", "Ensalada"],
-        correctAnswer: "Sushi"
+        question: "¿Cuál es tu color favorito?",
+        options: ["Verde", "Naranjo", "Amarillo", "Celeste"],
+        correctAnswer: "Verde"
     },
     {
         id: 3,
-        question: "¿Cual es mi postre favorito?",
-        options: ["Helado", "Brownie", "Cheesecake", "Fruta"],
-        correctAnswer: "Cheesecake"
-    }
+        question: "¿Cuál es mi comida chatarra favorita?",
+        options: ["Papas Fritas", "Sushi", "Pizza", "Completos"],
+        correctAnswer: "Pizza"
+    },
+    {
+        id: 4,
+        question: "No te gusta matar insectos excepto cuando son:",
+        options: ["Arañas", "Moscas", "Hormigas", "Chanchitos de tierra"],
+        correctAnswer: "Moscas"
+    },
+    {
+        id: 5,
+        question: "¿Cuál es el segundo nombre de la Ema?",
+        options: ["Amanda", "Sofia", "Elizabeth", "No tiene"],
+        correctAnswer: "Sofia"
+    },
+    {
+        id: 6,
+        question: "¿Cuál banda chilena no me gusta?",
+        options: ["Los Bunkers", "Los tres", "De Saloon", "Fother Muckers"],
+        correctAnswer: "Fother Muckers"
+    },
+    {
+        id: 7,
+        question: "¿Qué canción toca tu caja musical?",
+        options: ["Fix you", "Yellow", "Sparks", "The Scientist"],
+        correctAnswer: "Yellow"
+    },
+    {
+        id: 8,
+        question: "¿Qué canción sonó cuando fuimos al Takes?",
+        options: ["Magic", "Yellow", "Sparks", "The Scientist"],
+        correctAnswer: "Yellow"
+    },
+    {
+        id: 9,
+        question: "¿De qué lugar era el llavero que se te perdió?",
+        options: ["Cancún", "Florianapolis", "Mar de plata", "Bariloche"],
+        correctAnswer: "Mar de plata"
+    },
 ];
